6bf49e131b2a1f7c6c381cd6d330621ab10bc2f9
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-07-29 19:46:32 +0300

    creates a function that enables the computer to keep drawing cards when its score is below 17

--- index.py
+++ index.py
@@ -32,5 +32,14 @@ def calculate_score(cards):
     print(f" Computer's first card: {computer_cards[0]}")
     if user_score == 0 or computer_score ==0 or user_score > 21:
         is_game_over = True
-
-     
+    else:
+        user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+        if user_should_deal == "y":
+            user_cards.append(deal_card())
+        else:
+            is_game_over = True
+            
+    while computer_score !=0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+        
