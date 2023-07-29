529528a5a4189e197260a1311415d68e357ae5e3
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-07-29 17:47:19 +0300

    terminates the program once we display cards beyond 21

--- index.py
+++ index.py
@@ -13,8 +13,10 @@ computer_cards = []
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
+    is_game_over = False
 
 def calculate_score(cards):
+   
     if 11 in cards and 10 in cards and len(cards) == 2:
         return 0
     
@@ -24,5 +26,11 @@ def calculate_score(cards):
         cards.append(1)
         return sum(cards)
 
-# checking for a Blackjack
-#     
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f" Your cards: {user_cards}, current score: {user_score}")
+    print(f" Computer's first card: {computer_cards[0]}")
+    if user_score == 0 or computer_score ==0 or user_score > 21:
+        is_game_over = True
+
+     
